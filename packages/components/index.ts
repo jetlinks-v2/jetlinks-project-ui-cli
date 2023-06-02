0887f41f3006a59320439171baa6ba6f658545bd
XieYongHong <18010623010@163.com>
2023-06-02 16:10:19 +0800

    feat: 新增FullPage组件

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -2,6 +2,8 @@ import type { App } from 'vue'
 export { default as BlankLayoutPage} from './layout/BlankLayoutPage.vue'
 export { default as Iframe} from './layout/Iframe.vue'
 export { default as BasicLayoutPage} from './layout/BasicLayoutPage.vue'
+
+import FullPage from './FullPage/index.vue'
 import PermissionButton from './PermissionButton/index'
 import { PageContainer, AIcon } from 'jetlinks-ui-components'
 
@@ -10,5 +12,6 @@ export default {
     app.component('PageContainer', PageContainer)
       .component('AIcon', AIcon)
       .component('PermissionButton', PermissionButton)
+      .component('FullPage', FullPage)
   }
 }
