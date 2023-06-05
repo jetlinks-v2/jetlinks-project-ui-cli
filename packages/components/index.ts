f8d888d89deb0fbbbd50ce476b00f67b1d15db87
XieYongHong <18010623010@163.com>
2023-06-05 18:43:17 +0800

    feat: 添加.npmrc;优化package.json

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,9 +1,10 @@
 import type { App } from 'vue'
 
-import FullPage from './FullPage/index.vue'
-import PermissionButton from './PermissionButton/index'
+import FullPage from './src/FullPage/index.vue'
+import PermissionButton from './src/PermissionButton/index'
+import BadgeStatus from './src/BadgeStatus'
+
 import { PageContainer, AIcon } from 'jetlinks-ui-components'
-import BadgeStatus from './BadgeStatus'
 
 export default {
   install(app: App) {
