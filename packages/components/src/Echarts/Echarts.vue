9af894945938eaba512f34677d263270b7466172
100011797 <2642441182@qq.com>
2023-07-31 10:48:38 +0800

    fix: 修改组件，封装echarts

---
+++ packages/components/src/Echarts/Echarts.vue
@@ -0,0 +1,43 @@
+<template>
+  <div ref="echartsDom" class="echarts-warp" :style="style"></div>
+</template>
+  
+  <script lang="ts" name="Echarts" setup>
+import { CSSProperties, nextTick, ref, watch, Ref } from 'vue'
+import { useECharts } from '@jetlinks/hooks'
+
+interface Props {
+  style?: CSSProperties
+}
+
+const props = defineProps({
+  options: {
+    type: Object,
+    default: undefined,
+  },
+  style: Object as PropType<Props['style']>,
+})
+
+const echartsDom = ref<Ref<HTMLDivElement> | HTMLDivElement>()
+
+const { setOptions } = useECharts(echartsDom.value)
+
+watch(
+  () => props.options,
+  () => {
+    if (props.options) {
+      nextTick(() => {
+        setOptions(props.options)
+      })
+    }
+  },
+  { immediate: true, deep: true },
+)
+</script>
+  
+<style scoped>
+.echarts-warp {
+  width: 100%;
+  height: 100%;
+}
+</style>
