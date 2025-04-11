d7a2bd585d01b28b343660867b00d5be65b039c6
FuHao <62583365+haodd0216@users.noreply.github.com>
2025-04-11 16:37:46 +0800

    fix(FlameGraph): 优化样式和代码结构，移除冗余字段 (#27)
    * fix(FlameGraph): 优化样式和代码结构，移除冗余字段

--- packages/components/src/FlameGraph/FlameGraph.vue
+++ packages/components/src/FlameGraph/FlameGraph.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="j-flame-graph">
-    <a-space>
-      <a-input-search class="j-flame-graph-search" placeholder="请输入" @search="search"></a-input-search>
-      <a-button @click="invert">反转</a-button>
-    </a-space>
+    <div class="actions">
+      <a-space>
+        <a-input-search class="j-flame-graph-search" placeholder="请输入" @search="search"></a-input-search>
+        <a-button @click="invert">反转</a-button>
+      </a-space>
+    </div>
     <div ref="flameGraphRef"></div>
   </div>
 </template>
 <script setup>
 import { flamegraph } from 'd3-flame-graph'
 import * as d3 from 'd3';
-import { onMounted, watch, ref, computed } from 'vue';
+import { nextTick, watch, ref, computed } from 'vue';
 
 defineOptions({
   name: 'JFlameGraph',
@@ -27,14 +29,6 @@ const props = defineProps({
   showInvert:{
     type: Boolean,
     default: true
-  },
-  fieldNames: {
-    type: Object,
-    default: () => ({
-      name: 'className',
-      value: 'samples',
-      children: 'children'
-    })
   },
   request: {
     type: Function,
@@ -56,19 +50,15 @@ const search = (val) => {
 const formatData = computed(() => {
   return (val) => {
     function formatDataFn(newVal) {
-      newVal.name = newVal[props.fieldNames.name]
-      newVal.value = newVal[props.fieldNames.value]
-      if(newVal[props.fieldNames.children] || newVal.children) {
-        newVal[props.fieldNames.children || 'children'].forEach(item => {
-          formatDataFn(item)
-        })
-      }
+      newVal.children?.forEach(item => {
+        formatDataFn(item)
+      })
     }
     //格式化数据，增加根节点root
     const obj = {
-      [props.fieldNames.name]: 'root',
-      [props.fieldNames.value]: val.map(item => item[props.fieldNames.value]).reduce((a, b) => a + b, 0),
-      [props.fieldNames.children || 'children']: val
+      name: 'root',
+      value: val.map(item => item.value).reduce((a, b) => a + b, 0),
+      children: val
     }
     formatDataFn(obj)
     return obj
@@ -82,15 +72,15 @@ const invert = () => {
       .datum(formatData.value(props.data))
       .call(chart)
 }
-onMounted(() => {
-  watch(() => props.data, (newVal, oldVal) => {
-    if(newVal) {
+watch(() => props.data, (newVal, oldVal) => {
+  if(newVal) {
+    nextTick(() => {
       d3.select(flameGraphRef.value)
-      .datum(formatData.value(newVal))
-      .call(chart)
-    }
-  }, {immediate: true})
-})
+        .datum(formatData.value(newVal))
+        .call(chart)
+    })
+  }
+}, {immediate: true})
 
 </script>
 
