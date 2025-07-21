580244ba03361c8219ecb60536ca3804402c52c9
XieYongHong <18010623010@163.com>
2025-07-21 09:56:33 +0800

    fix: 优化hooks的低版本兼容

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -5,3 +5,7 @@ export * from './useIcon'
 export * from './usePermission'
 export * from './useRequest'
 export * from './useSmartRequest'
+
+export const useECharts = () => {
+  console.warn('[useECharts] 1.1版本开始废弃')
+}
