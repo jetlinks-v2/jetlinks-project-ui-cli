8bfbcb56f7a4bcd965026f0137ffce0df058f009
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2025-08-11 10:32:19 +0800

    feat: protable的卡片添加全选功能 (#41)

--- packages/components/src/ProTable/Content.vue
+++ packages/components/src/ProTable/Content.vue
@@ -2,8 +2,13 @@
   <div class="jtable-box">
     <template v-if="mode === 'CARD'">
       <div class="jtable-card">
+        <div style="margin-bottom: 10px" v-if="__rowSelection && __rowSelection.type === 'checkbox'">
+          <a-checkbox :indeterminate="indeterminate" :checked="checkedAll"
+                      @change="handleCheckedAllChange">{{ contextLocale.select.all }}
+          </a-checkbox>
+        </div>
         <div class="jtable-card-items" :style="{ gridTemplateColumns }" v-if="dataSource.length">
-          <div :class="['jtable-card-item', props.cardBodyClass]" v-for="item in dataSource" :key="item[props.rowKey]" >
+          <div :class="['jtable-card-item', props.cardBodyClass]" v-for="item in dataSource" :key="item[props.rowKey]" @click="onClick(item)">
             <slot name="card" v-bind="item"></slot>
           </div>
         </div>
@@ -15,7 +20,7 @@
       </div>
     </template>
     <template v-else>
-      <Table v-bind="props" :row-selection="rowSelection" :dataSource="dataSource" :columns="_columns" :pagination="false" :scroll="_scroll" :class="{'j-table-scroll': !props.scroll?.y}">
+      <Table v-bind="props" :row-selection="__rowSelection" :dataSource="dataSource" :columns="_columns" :pagination="false" :scroll="_scroll" :class="{'j-table-scroll': !props.scroll?.y}">
         <template v-for="(_, slotKey) in _slots" :key="slotKey" v-slot:[slotKey]="slotProps">
           <template v-if="!((column?.key || column?.dataIndex) && column?.scopedSlots && (_slots?.[column?.dataIndex] || _slots?.[column?.key]))">
             <slot :name="slotKey" v-bind="slotProps"></slot>
@@ -43,6 +48,8 @@ import {_contentProps} from "./setting";
 import {Table} from 'ant-design-vue';
 import {get, omit} from 'lodash-es';
 import Empty from '../Empty';
+import {useTableInject} from './hooks'
+import {useLocaleReceiver} from "../LocaleReciver";
 
 defineOptions({
   name: 'Content'
@@ -56,7 +63,8 @@ const props = defineProps({
   }
 })
 const slots = useSlots()
-
+const [contextLocale] = useLocaleReceiver('ProTable');
+const _rowSelection = useTableInject()
 
 const _columns = computed(() => props.columns.filter((i) => !i?.hideInTable))
 
@@ -81,5 +89,28 @@ const _scroll = computed(() => {
 const gridTemplateColumns = computed(() => {
   return `repeat(${props.column}, 1fr)`
 })
+
+const __rowSelection = computed(() => {
+  return props.rowSelection || _rowSelection?.value
+})
+
+const indeterminate = computed(() => {
+  return __rowSelection.value?.selectedRowKeys?.length > 0 && __rowSelection.value?.selectedRowKeys?.length < props.dataSource.length
+})
+
+const checkedAll = computed(() => {
+  return __rowSelection.value?.selectedRowKeys?.length > 0 && __rowSelection.value?.selectedRowKeys?.length === props.dataSource.length
+})
+
+const onClick = (item) => {
+  if(_rowSelection && _rowSelection.value) {
+    const _selected = _rowSelection.value.selectedRowKeys?.includes(item[props.rowKey])
+    _rowSelection.value.onSelect?.(item, !_selected)
+  }
+}
+const handleCheckedAllChange = (e) => {
+  const flag = e.target.checked;
+  __rowSelection.value?.onSelectAll?.(flag, props.dataSource, props.dataSource)
+}
 
 </script>
