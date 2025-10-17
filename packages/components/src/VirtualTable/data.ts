31e24cf8aadf965617527edacb3575c62c4577eb
sunchao <2642441182@qq.com>
2025-10-17 14:46:35 +0800

    fix: 树形表格没有返回children的问题

--- packages/components/src/VirtualTable/data.ts
+++ packages/components/src/VirtualTable/data.ts
@@ -2,32 +2,36 @@
 import {omit} from "lodash-es";
 
 export const flattenTree = (tree: any[], level = 0, parentId = null) => {
-    return tree.flatMap((item: any) => {
-        const flatNode = {
-            ...omit(item, 'children'),
-            level,
-            parentId,
-            expanded: false,
-            hasChildren: item.children?.length > 0,
-            visible: level === 0
-        }
-        const children: any[] = item.children ? flattenTree(item.children, level + 1, item.id) : []
-        return [flatNode, ...children]
-    })
+  return tree.flatMap((item: any) => {
+    const _children = (item.children || []).map(i => ({
+      ...omit(i, 'children')
+    }))
+    const flatNode = {
+      ...omit(item, 'children'),
+      children: _children, // 为了需要判断孩子的长度的时候使用
+      level,
+      parentId,
+      expanded: false,
+      hasChildren: item.children?.length > 0,
+      visible: level === 0,
+    }
+    const children: any[] = item.children ? flattenTree(item.children, level + 1, item.id) : [];
+    return [flatNode, ...children]
+  })
 }
 
 export const findAllChildren = (data = [], parentId = null) => {
-    let result = [];
+  let result = [];
 
-    function recursive(pid) {
-        data.forEach(item => {
-            if (item.parentId === pid) {
-                result.push(item);
-                recursive(item.id); // 递归查找这个子节点的孩子
-            }
-        });
-    }
+  function recursive(pid) {
+    data.forEach(item => {
+      if (item.parentId === pid) {
+        result.push(item);
+        recursive(item.id); // 递归查找这个子节点的孩子
+      }
+    });
+  }
 
-    recursive(parentId);
-    return result;
+  recursive(parentId);
+  return result;
 }
